748fdbce4e4f210f2ba215d3cc5ad7946c878ffb
amilali <66510886+amilali@users.noreply.github.com>
2021-06-26 20:48:18 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>Copyright By &copy; ShapeAi {new Date().getFullYear()}</p>
+    </footer>
+  );
+}
+
+export default Footer;
